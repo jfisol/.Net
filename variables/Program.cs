2b101ed4a1b23490cf633bf801b8b99eab377d4d
Jose Figueroa <taodejoju@gmail.com>
2024-04-13 16:31:13 -0600

    update de tema variables

--- variables/Program.cs
+++ variables/Program.cs
@@ -1,3 +1,21 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
-Console.ReadLine();
+Console.WriteLine("VARIABLES");
+
+
+//variables numéricas
+int numero = 10;
+
+//variables de cadenas de texto
+
+string cadena = 'Hola mundo';
+
+//Linea de escritura
+
+Console.WriteLine($"Valor numerico: {numero} \n Cadenas de texto: {cadena}");
+
+//Linea de lectura
+Console.ReaLine( );
+
+
+
+
---
+++ variables/README.md
@@ -0,0 +1,5 @@
+# Variable
+
+- Una variable es un contenedor de información que se puede utilizar para almacenar un valor en memoria.
+- Las variables pueden ser de diferentes tipos: númerica, cadenas de texto o booleanos, etc.
+- La información que almacenan se usa en los programas.
